0aa0d567ed44b7793cf7ebc136a54646bb234fde
tingyouwu <wutingyou@wxchina.com>
2016-11-25 13:00:38 +0800

    修改代码

--- WifiRGB2/app/src/main/java/com/wty/app/wifirgb/activity/NetWorkSetttingActivity.java
+++ WifiRGB2/app/src/main/java/com/wty/app/wifirgb/activity/NetWorkSetttingActivity.java
@@ -3,7 +3,6 @@ package com.wty.app.wifirgb.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
--- WifiRGB2/wcolorpicker/src/main/java/com/wcolorpicker/android/WCircleColorPicker.java
+++ WifiRGB2/wcolorpicker/src/main/java/com/wcolorpicker/android/WCircleColorPicker.java
@@ -1,7 +1,6 @@
 package com.wcolorpicker.android;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,7 +9,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
