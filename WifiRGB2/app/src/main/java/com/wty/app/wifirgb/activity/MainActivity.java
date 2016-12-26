380c7d13f5b7582fe2fef5aa4fcc48435378d9f2
tingyouwu <2421818708@qq.com>
2016-12-26 23:18:14 +0800

    优化代码

--- WifiRGB2/app/src/main/java/com/wty/app/wifirgb/activity/MainActivity.java
+++ WifiRGB2/app/src/main/java/com/wty/app/wifirgb/activity/MainActivity.java
@@ -77,9 +77,9 @@ public class MainActivity extends AppCompatActivity implements IOnColorChangeLis
     public void onColorSelected(int red, int green, int blue) {
         tv_color.setText("R:"+red+",G:"+green+",B:"+blue);
         getSendMessage(red,green,blue);
-//        handler.removeCallbacks(sendThread);
-        sendMessage(sendMessage);
-//        handler.postDelayed(sendThread, 0);
+        handler.removeCallbacks(sendThread);
+//        sendMessage(sendMessage);
+        handler.postDelayed(sendThread, 10);
     }
 
     @Override
