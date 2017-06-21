ccccb3615e8b2b5e6cfd655d9b96b3b8aab32ae6
tingyouwu <wutingyou@wxchina.com>
2017-06-21 11:05:09 +0800

    修改代码

--- WifiRGB2/bluetoothlib/src/main/java/com/wty/app/bluetoothlib/ble/adapter/BleDeviceListAdapter.java
+++ WifiRGB2/bluetoothlib/src/main/java/com/wty/app/bluetoothlib/ble/adapter/BleDeviceListAdapter.java
@@ -1,10 +1,12 @@
 package com.wty.app.bluetoothlib.ble.adapter;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import com.wty.app.bluetoothlib.R;
 import com.wty.app.bluetoothlib.ble.data.ScanResult;
+import com.wty.app.bluetoothlib.util.RssiUtil;
 
 import java.util.List;
 
@@ -15,12 +17,22 @@ import java.util.List;
 public class BleDeviceListAdapter extends BaseViewCommonAdapter<ScanResult> {
 
     public BleDeviceListAdapter(Context context, List<ScanResult> data){
-        super(context, R.layout.device_name,data);
+        super(context, R.layout.device_name_ble,data);
     }
 
     @Override
     protected void convert(BaseViewHolder holder, ScanResult item, int position) {
         TextView tv_name = holder.getView(R.id.tv_name);
-        tv_name.setText(item.getDevice().getName()+"\n" + item.getDevice().getAddress());
+        TextView tv_distance = holder.getView(R.id.tv_distance);
+        TextView tv_mac = holder.getView(R.id.tv_mac);
+
+        if(TextUtils.isEmpty(item.getDevice().getName())){
+            tv_name.setText("未知");
+        }else{
+            tv_name.setText(item.getDevice().getName());
+        }
+
+        tv_distance.setText("距离："+ RssiUtil.getDistance(item.getRssi())+"m");
+        tv_mac.setText("MAC地址："+item.getDevice().getAddress());
     }
 }
---
+++ WifiRGB2/bluetoothlib/src/main/java/com/wty/app/bluetoothlib/util/RssiUtil.java
@@ -0,0 +1,26 @@
+package com.wty.app.bluetoothlib.util;
+
+import java.math.BigDecimal;
+
+/**
+ * 功能：根据rssi计算距离
+ **/
+
+public class RssiUtil {
+    //A和n的值，需要根据实际环境进行检测得出
+    private static final double A_Value=50;/**A - 发射端和接收端相隔1米时的信号强度*/
+    private static final double n_Value=2.5;/** n - 环境衰减因子*/
+
+    /**
+     * 根据Rssi获得返回的距离,返回数据单位为m
+     * @param rssi
+     * @return
+     */
+    public static double getDistance(int rssi){
+        int iRssi = Math.abs(rssi);
+        double power = (iRssi-A_Value)/(10*n_Value);
+        double result =  Math.pow(10,power);
+        BigDecimal b = new BigDecimal(result);
+        return b.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+}
